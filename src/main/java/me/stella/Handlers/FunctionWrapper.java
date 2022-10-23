c9ca980c328461cc52eb120526018d80978d5341
Stella (DucTrader) <63104132+emnoigi1452@users.noreply.github.com>
2022-10-23 10:20:06 +0700

    Made all functions be executed via the scheduler

--- src/main/java/me/stella/Handlers/FunctionWrapper.java
+++ src/main/java/me/stella/Handlers/FunctionWrapper.java
@@ -3,6 +3,7 @@ package me.stella.Handlers;
 import java.util.function.Consumer;
 
 import org.bukkit.event.Event;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitScheduler;
 
 import me.stella.Main.SealPlugin;
@@ -25,16 +26,16 @@ public class FunctionWrapper {
 		this.scheduler = SealPlugin.instance.getServer().getScheduler();
 	}
 	
-	public void execute(Event event) throws Exception {
+	public void execute(final Event event) throws Exception {
 		final Consumer<Event> reference = this.function;
-		if(this.async) {
-			Runnable r = new Runnable() {
-				@Override
-				public void run() {
-					reference.accept(event);
-				}
-			};
-			this.scheduler.runTaskAsynchronously(SealPlugin.instance, r);
-		} else reference.accept(event);
+		BukkitRunnable run = new BukkitRunnable() {
+			public void run() {
+				reference.accept(event);
+			}
+		};
+		if(this.async)
+			run.runTaskAsynchronously(SealPlugin.instance);
+		else
+			run.runTask(SealPlugin.instance);
 	}
 }
